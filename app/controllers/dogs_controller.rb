4aa3afad44924ccb12579a6093ca2aa6161d6971
github username <github email address>
2020-04-09 17:29:04 -0700

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,32 @@
 class DogsController < ApplicationController
+    before_action :set_dog, only: [:show, :edit, :update]
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.create(dog_params)
+        byebug
+        redirect_to dog_path(@dog)
+    end
+
+    def update
+        @dog.update(dog_params)
+        redirect_to @dog
+    end
+
+    private
+
+    def set_dog
+        @dog = Dog.find(params[:id])
+    end
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age, employee_ids: [])
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,40 @@
 class EmployeesController < ApplicationController
+    before_action :set_employee, only: [:show, :edit, :update]
+
+    def index
+        @employees = Employee.all
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to @employee
+        else
+            flash[:messages] = @employee.errors.full_messages
+            render :new
+        end
+    end
+
+    def update
+        if @employee.update(employee_params)
+            redirect_to @employee
+        else
+            flash[:messages] = @employee.errors.full_messages
+            render :edit
+        end
+    end
+
+    private
+
+    def set_employee
+        @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id)
+    end
 end
