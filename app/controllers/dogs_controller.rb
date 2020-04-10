9e63a7d5c56c63c32d451e9c349154e67074fb1e
christine <bumpouce@gmail.com>
2020-04-09 20:22:05 -0700

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,21 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+        @employees = @dog.employees
+    end
+
+
+
+    # private
+
+    # def dog_params
+    #     params.require(:dog).permit!
+    # end
+
+
 end
